eb497a405db93b2b173f907bd46ca10a96db20ae
juanpablomg17 <58058546+juanpablomg17@users.noreply.github.com>
2020-03-25 23:53:03 -0500

    la misma wea de que faltan campos en formulario

--- src/components/Asistencias/React/UnaAsistencia.jsx
+++ src/components/Asistencias/React/UnaAsistencia.jsx
@@ -1,44 +1,66 @@
-import React from 'react'
+
 import '../Css/Asistencia.css';
 
-const Asistencia = (props) => {
 
-    const {id,title, responsible,description,priority} = props.asistencia;
+import React, { Component } from 'react'
+
+export default class UnaAsistencia extends Component {
+
+
+  Asistencia = (props) => {
+    const { id, title, responsible, description, priority } = this.props.asistencia;
     return(
-        
-           <div className="col-md-6">
-           <div className="card" id="tajeta">
-            
-            <div className="card mt-4">
-              <div className="card-title text-center mb-5">
-                <h3>{title}</h3>
-                <span className="badge badge-pill badge-danger ml-2 mb-5">
-                  {priority}
-                </span>
-              </div>
-              <div className="card-body text-center">
-                <div>{description}</div> 
-                <div>{responsible}</div>
-                
-              </div>
-              <div className="card-footer">
-                <button id="boton" 
-                  className="btn btn-danger"
-                  >
-                  Delete
-                </button>
-              </div>
-            </div>
+      <div className="col-md-12">
+           
+         
+         <div className="card" id="tajeta">
+   
+           <div className="card mt-4">
+             <div className="card-title text-center mb-5">
+               <h3>{title}</h3>
+               <span className="badge badge-pill badge-danger ml-2 mb-5">
+                 {priority}
+               </span>
+             </div>
+             <div className="card-body text-center">
+               <div>{description}</div>
+               <div>{responsible}</div>
+   
+             </div>
+             <div className="card-footer">
+               <button id="boton"
+                 className="btn btn-danger"
+                 onClick={this.props.DeleteAsistencia.bind(this,id)}>
+                 Delete
+                 </button>
+             </div>
+           </div>
+         </div>
+         </div>
+      
+     
+
+
+  );
+  }
+
+  render() {
+
+   
+   
+    return (
+
           
-      </div>
+                <div >
+                      {this.Asistencia()}
+                </div>
+                
+              
 
-        </div>
+         
 
-        
-       
-       
+
+
     )
+  }
 }
-
-
-export default Asistencia;
